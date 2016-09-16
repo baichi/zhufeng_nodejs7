dc66b6bdba77b849dd66609e7f609473e133cfa5
zhufengzhufeng <1035465284@qq.com>
2016-09-16 15:46:44 +0800

    listener

--- 7.events/1.events.js
+++ 7.events/1.events.js
@@ -14,10 +14,22 @@ function die(who) {
 }
 //addListener方法和on一样
 girl.once('我要没钱了',noMoney);
+//设置最大监听数，默认为10个,不会阻断程序执行，只是抛出警告
+girl.setMaxListeners(2);
+girl.on('我要没钱了',die);
+girl.on('我要没钱了',die);
 girl.on('我要没钱了',die);
 //发射事件
-
 //删除绑定的事件,删除单一绑定
-girl.removeListener('我要没钱了',die);
+// girl.removeListener('我要没钱了',die);
+//移除所有监听
+//girl.removeAllListeners('我要没钱了');
+//获取对应的事件池
+console.log(girl.listeners('我要没钱了'));
+//获取监听数量
+console.log(girl.listenerCount('我要没钱了'));
 girl.emit('我要没钱了','红太郎');
 girl.emit('我要没钱了','红太郎');
+console.log(girl.listenerCount('我要没钱了'));
+// 主要的api
+//on emit once removeListener
